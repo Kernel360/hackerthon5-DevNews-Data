7d82a2f7ec61c92e930965de08596f9b28958ae3
devyoon <wideskyinme@gmail.com>
2025-05-08 13:47:05 +0900

    refactor: CategoryRepository 파일 이동

--- src/main/java/lenrek/data_crawling/repository/Category/CategoryRepository.java
+++ src/main/java/lenrek/data_crawling/repository/Category/CategoryRepository.java
@@ -1,5 +1,7 @@
-package lenrek.data_crawling.domain.category;
+package lenrek.data_crawling.repository.Category;
 
+import lenrek.data_crawling.domain.category.Category;
+import lenrek.data_crawling.domain.category.CategoryEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
